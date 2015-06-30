215cc3f356953d005e1b71844c3811d6da418099
archerImagine <archerimagine@gmail.com>
2015-06-30 23:42:26 +0530

    error correction

--- mySite/publishconf.py
+++ mySite/publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://archerimagine.github.io/myBlogDevelopment/mySite'
+SITEURL = 'http://archerimagine.github.io/myBlogDevelopment/mySite/output'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
