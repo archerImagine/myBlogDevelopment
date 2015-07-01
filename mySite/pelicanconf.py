f815a1412d9657163a19346841525a095c88a525
archerImagine <archerimagine@gmail.com>
2015-07-01 23:11:04 +0530

    UPDATED TO PELICAN ELEGANT THEME

--- mySite/pelicanconf.py
+++ mySite/pelicanconf.py
@@ -18,7 +18,7 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
+THEME = '/Users/animeshbhadra/myWorks/myGithubCode/myWebsite/pelican-elegant'
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
